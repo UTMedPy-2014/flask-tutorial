a8f3fe00e126f75d6e872531d7df23ecf72c7654
Sophiradex_A <t1dramaticsh@gmail.com>
2018-02-09 14:54:40 +0900

    # changed to sqlite

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pygresql://bgkiufczunefhd:51ec6c68a39fb898f7ac65852310738342b0f0dee77791bdd966917ea4ce754d@ec2-184-72-228-128.compute-1.amazonaws.com:5432/d2vp2fv24j2sc9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
--- models/lock/end/end.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-app = Blueprint("end", __name__, url_prefix="lock/end")
