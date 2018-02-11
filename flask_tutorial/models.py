f50abf12c7977cf7f1dc8d9ae35f8ddf391954ff
Sophiradex_A <t1dramaticsh@gmail.com>
2018-02-11 14:28:06 +0900

    # added account database

--- flask_tutorial/models.py
+++ flask_tutorial/models.py
@@ -6,6 +6,13 @@ class User(db.Model):
 
     def __init__(self, username):
         self.username = username
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True)
+    #password = db.Column(db.String(80), unique=True)
+
+    def __init__(self, email):
+        self.email = email
 
 def init():
     db.create_all()
