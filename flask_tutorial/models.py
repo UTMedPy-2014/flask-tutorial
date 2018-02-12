164f056a0852a2740e9c66e9e5dc780fb32459b5
Sophiradex_A <t1dramaticsh@gmail.com>
2018-02-12 12:41:47 +0900

    # passwordの暗号化 - tools/rsa.pyの追加 - flask_tutorial.toolsからrsaをviewsにimport

--- flask_tutorial/models.py
+++ flask_tutorial/models.py
@@ -4,11 +4,11 @@ class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80), unique=True)
-    def __init__(self, username, email, password):
+    password_enc = db.Column(db.String(80), unique=True)
+    def __init__(self, username, email, password_enc):
         self.username = username
         self.email = email
-        self.password = password
+        self.password_enc = password_enc
 
 def init():
     db.create_all()
--- flask_tutorial/views.py
+++ flask_tutorial/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
 from flask_tutorial import app, db
 from flask_tutorial.models import Account
+from flask_tutorial.tools import rsa
 
 @app.route("/")
 def index():
@@ -11,9 +12,11 @@ def index():
 def add_account():
     username = request.form.get('username')
     email = request.form.get('email')
-    password = request.form.get('password')
-    if username and email and password:
-        account = Account(username, email, password)
+    public_key, private_key = rsa.generate_keys(103, 3259)#65521
+    password_enc = rsa.encrypt(request.form.get('password'), public_key)
+    if username and email and password_enc:
+        account = Account(username, email, password_enc)
         db.session.add(account)
         db.session.commit()
+        print(rsa.sanitize(rsa.decrypt(password_enc, private_key)))
     return redirect(url_for('index'))
